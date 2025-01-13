08de950c7c2cd9b36f4501cd928504d9425735b7
MD Nafizul Iqram <nafizulikram3@gmail.com>
2025-01-13 13:26:15 +0600

    Sign up method and image upload implimented

---
+++ src/Api/Utils.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+
+// Upload image and return image url 
+export const imageUpload = async (imageData) => {
+
+    const formData = new FormData()
+    formData.append('file', imageData);
+    formData.append('upload_preset', 'nafiz_vai_cloudinary');
+
+    const { data } = await axios.post(`https://api.cloudinary.com/v1_1/${import.meta.env.VITE_CLOUDINARY_NAME}/image/upload`, formData)
+
+    return data.secure_url
+}
